bc5af64c3012dfb373868d60d5f048f05c0fecf0
Alan Silva <alanfortlink@gmail.com>
2024-03-17 13:56:29 +0000

    Move word management to WordShooter

--- game.js
+++ game.js
@@ -1,17 +1,17 @@
 import Word from './word.js';
 import GameUI from './game_ui.js';
 import Vec2 from './vec2.js';
+import WordShooter from './word_shooter.js';
 
 class Game {
-  constructor(words) {
+  constructor(dictionary) {
     this.canvas = document.getElementById("gameCanvas");
     this.context = this.canvas.getContext("2d");
     this.score = 0;
     this.duration = 30;
     this.gameElapsed = 0;
     this.gameRunning = false;
-    this.world = [];
-    this.words = words;
+    this.wordShooter = new WordShooter(this.canvas, dictionary);
   }
 
   updateOverlay() {
@@ -27,45 +27,6 @@ class Game {
     GameUI.updateInfo(timeLeft, this.score);
   }
 
-  spawnNewWord() {
-    if (this.world.length > 3) {
-      return;
-    }
-
-    const width = this.canvas.width;
-    const height = this.canvas.height;
-
-    var word = null;
-
-    // Select a word that doesn't conflict with the initials of the existing words
-    while (true) {
-      const cand = this.words[parseInt(Math.random() * this.words.length)];
-      const initials = this.world.map(w => w.getRemainingWord()[0]);
-      if (!initials.includes(cand[0])) {
-        word = cand;
-        break;
-      }
-    }
-
-    const launch_x = 0.5 * Math.random() * width + width / 4;
-
-    const min_velocity = 0.55 * height;
-    const random_velocity = Math.random() * 0.35 * height;
-
-    const throwAngleLimit = 20;
-    const throwAngle = Math.random() * throwAngleLimit - throwAngleLimit / 2;
-
-    const position = new Vec2(launch_x, height + 20);
-    const velocity = (new Vec2(0, -1).rotated(throwAngle).mult(min_velocity + random_velocity));
-
-    const onRemoveCallback = (word) => {
-      this.world = this.world.filter(w => w !== word);
-      this.spawnNewWord();
-    };
-
-    this.world.push(new Word(word, position, velocity, onRemoveCallback));
-  }
-
   onkeydown(e) {
     if (!this.gameRunning) {
       return;
@@ -74,17 +35,17 @@ class Game {
     const c = e.key;
 
     if (c == "Backspace") {
-      for (let word of this.world) {
+      for (let word of this.wordShooter.words) {
         word.selected = false;
       }
     }
 
-    const selectedWords = this.world.filter(w => w.selected);
+    const selectedWords = this.wordShooter.words.filter(w => w.selected);
     var selectedWord = null;
     if (selectedWords.length > 0) {
       selectedWord = selectedWords[0];
     } else {
-      const words = this.world.filter(w => w.getRemainingWord().startsWith(c));
+      const words = this.wordShooter.words.filter(w => w.getRemainingWord().startsWith(c));
       if (words.length > 0) {
         words[0].selected = true;
         selectedWord = words[0];
@@ -99,7 +60,7 @@ class Game {
   }
 
   reset() {
-    this.world = [];
+    this.wordShooter.reset();
     this.gameRunning = true;
     this.gameElapsed = 0;
     this.score = 0;
@@ -115,19 +76,14 @@ class Game {
   update(dt) {
     if (this.gameRunning) {
       this.updateOverlay();
-      this.spawnNewWord();
       this.gameElapsed += dt;
       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      for (let word of this.world) {
-        word.update(dt);
-      }
+      this.wordShooter.update(dt);
     }
   }
 
   render() {
-    for (let word of this.world) {
-      word.render(this.context);
-    }
+    this.wordShooter.render(this.context);
   }
 
 }
---
+++ word_shooter.js
@@ -0,0 +1,84 @@
+import Word from './word.js';
+import Vec2 from './vec2.js';
+
+class WordShooter {
+  constructor(canvas, dictionary) {
+    this.words = [];
+    this.canvas = canvas;
+    this.dictionary = dictionary;
+    this.simultaneousWords = 5;
+    this.wordsWaiting = 0;
+    this.timeSinceLastWord = 0;
+    this.timeBetweenWords = 0.5;
+  }
+
+  updateWords(dt) {
+    if (this.words.length >= this.simultaneousWords) {
+      return;
+    }
+
+    if (this.timeSinceLastWord < this.timeBetweenWords) {
+      this.timeSinceLastWord += dt;
+      return;
+    }
+
+    this.wordsWaiting--;
+    this.timeSinceLastWord = 0;
+
+    const width = this.canvas.width;
+    const height = this.canvas.height;
+
+    var word = null;
+
+    // Select a word that doesn't conflict with the initials of the existing words
+    while (true) {
+      const cand = this.dictionary[parseInt(Math.random() * this.dictionary.length)];
+      const initials = this.words.map(w => w.getRemainingWord()[0]);
+      if (!initials.includes(cand[0])) {
+        word = cand;
+        break;
+      }
+    }
+
+    const launch_x = 0.5 * Math.random() * width + width / 4;
+
+    const min_velocity = 0.55 * height;
+    const random_velocity = Math.random() * 0.35 * height;
+
+    const throwAngleLimit = 20;
+    const throwAngle = Math.random() * throwAngleLimit - throwAngleLimit / 2;
+
+    const position = new Vec2(launch_x, height + 20);
+    const velocity = (new Vec2(0, -1).rotated(throwAngle).mult(min_velocity + random_velocity));
+
+    const onRemoveCallback = (word) => {
+      this.words = this.words.filter(w => w !== word);
+      this.wordsWaiting++;
+    };
+
+    this.words.push(new Word(word, position, velocity, onRemoveCallback));
+  }
+
+  update(dt) {
+    if (this.words.length + this.wordsWaiting < this.simultaneousWords) {
+      this.wordsWaiting++;
+    }
+
+    this.updateWords(dt);
+
+    for (let word of this.words) {
+      word.update(dt);
+    }
+  }
+
+  render(context) {
+    for (let word of this.words) {
+      word.render(context);
+    }
+  }
+
+  reset() {
+  }
+}
+
+export default WordShooter;
