632a7eb182db11205659ba052545f5fe83b0b56a
Alan Silva <alanfortlink@gmail.com>
2024-04-28 19:40:46 +0100

    Update

---
+++ ninjatype/bin/ninjatype.dart
@@ -0,0 +1,5 @@
+import 'package:ninjatype/ninjatype.dart' as ninjatype;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ninjatype.calculate()}!');
+}
---
+++ ninjatype/test/ninjatype_test.dart
@@ -0,0 +1,8 @@
+import 'package:ninjatype/ninjatype.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
